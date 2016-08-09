c7b86e145e8cac1349dd5081e771ba3ae32155f4
tharvik <tharvik@users.noreply.github.com>
2016-08-09 11:17:23 +0200

    20.6

--- stdlib/2and3/xml/dom/__init__.pyi
+++ stdlib/2and3/xml/dom/__init__.pyi
@@ -1,6 +1,7 @@
 # Stubs for xml.dom
 
 from typing import Callable, Optional, Sequence, Sized, Tuple
+from abc import abstractmethod
 
 
 def registerDOMImplementation(name: str,
@@ -14,16 +15,20 @@ XHTML_NAMESPACE = ...  # type: str
 
 
 class DomImplementation:
+    @abstractmethod
     def hasFeature(self, feature: str, version: str) -> bool: ...
+    @abstractmethod
     def createDocument(self, namespaceUri: Optional[str],
                        qualifiedName: Optional[str],
                        doctype: Optional[str]) -> Document: ...
+    @abstractmethod
     def createDocumentType(self, qualifiedName: str, publicId: str,
                            systemId: str) -> DocumentType: ...
 
 
 class Node:
     @property
+    @abstractmethod
     def nodeType(self) -> int: ...
     ELEMENT_NODE = ...  # type: int
     ATTRIBUTE_NODE = ...  # type: int
@@ -36,118 +41,201 @@ class Node:
     DOCUMENT_TYPE_NODE = ...  # type: int
     NOTATION_NODE = ...  # type: int
     @property
+    @abstractmethod
     def parentNode(self) -> Optional[Node]: ...
     @property
+    @abstractmethod
     def attributes(self) -> Optional[NamedNodeMap]: ...
     @property
+    @abstractmethod
     def previousSibling(self) -> Optional[Node]: ...
     @property
+    @abstractmethod
     def nextSibling(self) -> Optional[Node]: ...
     @property
+    @abstractmethod
     def childNodes(self) -> List[Node]: ...
     @property
+    @abstractmethod
     def firstChild(self) -> Optional[Node]: ...
     @property
+    @abstractmethod
     def lastChild(self) -> Optional[Node]: ...
-    localName = ...  # type: str
-    prefix = ...  # type: Optional[str]
     @property
+    @abstractmethod
+    def localName(self) -> str: ...
+    @property
+    @abstractmethod
+    def prefix(self) -> Optional[str]: ...
+    @property
+    @abstractmethod
     def namespaceURI(self) -> Optional[str]: ...
     @property
+    @abstractmethod
     def nodeName(self) -> Optional[str]: ...
-    nodeValue = ...  # type: Optional[str]
+    @property
+    @abstractmethod
+    def nodeValue(self) -> Optional[str]: ...
+    @abstractmethod
     def hasAttributes(self) -> bool: ...
+    @abstractmethod
     def hasChildNodes(self) -> bool: ...
+    @abstractmethod
     def isSameNode(self, other: Node) -> bool: ...
+    @abstractmethod
     def appendChild(self, newChild: Node) -> None: ...
+    @abstractmethod
     def insertBefore(self, newChild: Node, refChild: Optional[Node]) -> Node: ...
+    @abstractmethod
     def removeChild(self, oldChild: Node) -> Node: ...
+    @abstractmethod
     def replaceChild(self, newChild: Node, oldChild: Node) -> None: ...
+    @abstractmethod
     def normalize(self) -> None: ...
+    @abstractmethod
     def cloneNode(self, deep: bool) -> Node: ...
 
 
 class NodeList(Sized):
-    length = ...  # type: int
+    @property
+    @abstractmethod
+    def length(self) -> int: ...
+    @abstractmethod
     def item(self, i: int) -> Optional[Node]: ...
+    @abstractmethod
     def __getitem__(self, i: int) -> Optional[Node]: ...
 
 
 class DocumentType(Node):
-    publicId = ...  # type: Optional[str]
-    systemId = ...  # type: Optional[str]
-    internalSubset = ...  # type: Optional[str]
-    name = ...  # type: str
-    entities = ...  # type: Optional[NamedNodeMap]
-    notations = ...  # type: Optional[NamedNodeMap]
+    @property
+    @abstractmethod
+    def publicId(self) -> Optional[str]: ...
+    @property
+    @abstractmethod
+    def systemId(self) -> Optional[str]: ...
+    @property
+    @abstractmethod
+    def internalSubset(self) -> Optional[str]: ...
+    @property
+    @abstractmethod
+    def name(self) -> str: ...
+    @property
+    @abstractmethod
+    def entities(self) -> Optional[NamedNodeMap]: ...
+    @property
+    @abstractmethod
+    def notations(self) -> Optional[NamedNodeMap]: ...
 
 
 class Document(Node):
-    documentElement = ...  # type: Element
+    @property
+    @abstractmethod
+    def documentElement(self) -> Element: ...
+    @abstractmethod
     def createElement(self, tagName: str) -> Element: ...
+    @abstractmethod
     def createElementNS(self, namespaceURI: str, tagName: str) -> Element: ...
+    @abstractmethod
     def createTextNode(self, data: str) -> Text: ...
+    @abstractmethod
     def createComment(self, data: str) -> Comment: ...
+    @abstractmethod
     def createProcessingInstruction(self, target: str,
                                     data: str) -> ProcessingInstruction: ...
+    @abstractmethod
     def createAttribute(self, name: str) -> Attr: ...
+    @abstractmethod
     def createAttributeNS(self, namespaceURI: str,
                           qualifiedName: str) -> Attr: ...
+    @abstractmethod
     def getElementsByTagName(self, tagName: str) -> NodeList: ...
+    @abstractmethod
     def getElementsByTagNameNS(self, namespaceURI: str,
-            localName: str) -> NodeList: ...
+                               localName: str) -> NodeList: ...
 
 
 class Element(Node):
-    tagName = ...  # type: str
+    @property
+    @abstractmethod
+    def tagName(self) -> str: ...
+    @abstractmethod
     def getElementsByTagName(self, tagName: str) -> NodeList: ...
+    @abstractmethod
     def getElementsByTagNameNS(self, namespaceURI: str,
-            localName: str) -> NodeList: ...
+                               localName: str) -> NodeList: ...
+    @abstractmethod
     def hasAttribute(self, name: str) -> bool: ...
+    @abstractmethod
     def hasAttributeNS(self, namespaceURI: str, localName: str) -> bool: ...
+    @abstractmethod
     def getAttribute(self, name: str) -> str: ...
+    @abstractmethod
     def getAttributeNode(self, attrname: str) -> Attr: ...
+    @abstractmethod
     def getAttributeNS(self, namespaceURI: str, localName: str) -> str: ...
+    @abstractmethod
     def getAttributeNodeNS(self, namespaceURI: str, localName: str) -> Attr: ...
+    @abstractmethod
     def removeAttribute(self, name: str) -> None: ...
+    @abstractmethod
     def removeAttributeNode(self, oldAttr: Attr) -> None: ...
+    @abstractmethod
     def removeAttributeNS(self, namespaceURI: str, localName: str) -> None: ...
+    @abstractmethod
     def setAttribute(self, name: str, value: str) -> None: ...
+    @abstractmethod
     def setAttributeNode(self, newAttr: Attr) -> Node: ...
+    @abstractmethod
     def setAttributeNodeNS(self, newAttr: Attr) -> Node: ...
+    @abstractmethod
     def setAttributeNS(self, namespaceURI: str, qname: str,
                        value: Attr) -> None: ...
 
 
 class Attr(Node):
-    name = ...  # type: str
     @property
-    def localName(self) -> str: ...  # type: ignore
-    prefix = ...  # type: str
-    value = ...  # type: Optional[str]
+    @abstractmethod
+    def name(self) -> str: ...
+    @property
+    @abstractmethod
+    def prefix(self) -> str: ...
+    @property
+    @abstractmethod
+    def value(self) -> Optional[str]: ...
 
 
 class NamedNodeMap:
-    length = ...  # type: int
+    @property
+    @abstractmethod
+    def length(self) -> int: ...
+    @abstractmethod
     def item(self, index: int) -> Attr: ...
 
 
 class Comment(Node):
-    data = ...  # type: str
+    @property
+    @abstractmethod
+    def data(self) -> str: ...
 
 
 class Text(Node):
-    data = ...  # type: str
+    @property
+    @abstractmethod
+    def data(self) -> str: ...
 
 
 class ProcessingInstruction(Node):
     @property
+    @abstractmethod
     def target(self) -> str: ...
-    data = ...  # type: str
+    @property
+    @abstractmethod
+    def data(self) -> str: ...
 
 
 class DOMException(Exception):
     @property
+    @abstractmethod
     def code(self) -> int: ...
 
 class DomstringSizeErr(DOMException):
@@ -225,13 +313,3 @@ NO_DATA_ALLOWED_ERR = ...  # type: int
 NO_MODIFICATION_ALLOWED_ERR = ...  # type: int
 SYNTAX_ERR = ...  # type: int
 WRONG_DOCUMENT_ERR = ...  # type: int
-
-
-#class UserDataHandler:
-#    NODE_CLONED = ...  # type: Any
-#    NODE_IMPORTED = ...  # type: Any
-#    NODE_DELETED = ...  # type: Any
-#    NODE_RENAMED = ...  # type: Any
-
-#EMPTY_PREFIX = ...  # type: Any
-
